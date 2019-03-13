2083dc1b369aab14026c0722a96e6634a95bdaec
Othman ASSAS <o.assas@gop.link>
2019-03-13 14:40:12 +0100

    streamsReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
-import {combineReducers} from 'redux';
-import {reducer as formReducer} from 'redux-form';
-import authReducer from './authReducer';
+import { combineReducers } from "redux";
+import { reducer as form } from "redux-form";
+import auth from "./authReducer";
+import streams from "./streamsReducer";
 
 export default combineReducers({
-    auth:authReducer,
-    form:formReducer
-})
+  auth,
+  form,
+  streams
+});
