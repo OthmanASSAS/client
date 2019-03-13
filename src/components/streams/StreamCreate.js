9e4b79c74c4e28948a9c9dded09df6d774e2b7f5
Othman ASSAS <o.assas@gop.link>
2019-03-13 13:28:36 +0100

    crud operations

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+import {connect} from 'react-redux';
+import {createStream} from '../../actions'
 
 class StreamCreate extends Component {
   renderInput(formProps) {
@@ -29,7 +31,7 @@ class StreamCreate extends Component {
   };
 
   onSubmit = formValues => {
-    console.log(formValues);
+    this.props.createStream(formValues);
   };
 
   render() {
@@ -65,7 +67,9 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped)
