b3494e25e8dba4881571fa7607cea601804ba4f9
Othman ASSAS <o.assas@gop.link>
2019-03-13 10:14:05 +0100

    handling forms with redux form

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers/index";
 
-const createStoreWithApplyMiddleware = applyMiddleware()(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore( reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
-  <Provider
-    store={createStoreWithApplyMiddleware(
-      reducers,
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )}
-  >
+  <Provider store={store}>
     <App />
   </Provider>,
 
